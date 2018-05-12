3eb757d7eb1ab81f8f6f4eefd1b34dcfa155b087
christopher erdos <chris.erdos@gmail.com>
2018-05-12 11:21:08 -0500

    moved some things around, renamed some files, added footer to config

--- SpaceTravel/Configuration/UILayer/ConfigViewController.swift
+++ SpaceTravel/Configuration/UILayer/ConfigViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
+protocol ConfigViewControllerDelegate: class {
+	func didSaveConfig(_ config: Config)
+}
+
 class ConfigViewController: UIViewController {
 	var configDataSource: ConfigDataSource?
 	var config: Config?
+	weak var delegate: ConfigViewControllerDelegate?
 	
 	@IBOutlet weak var configTableView: UITableView!
 	
-	
-
     override func viewDidLoad() {
         super.viewDidLoad()
 		setup()
@@ -24,6 +27,26 @@ class ConfigViewController: UIViewController {
 	func setup() {
 		guard let config = config else { return }
 		configDataSource = ConfigDataSource(config: config, tableView: configTableView, delegate: self)
+		configTableView.tableFooterView = UIView()
+	}
+
+	
+	//MARK: - Actions
+	
+	@IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+		exitConfig()
+	}
+	
+	@IBAction func saveButtonTapped(_ sender: UIButton) {
+		if let config = config {
+			delegate?.didSaveConfig(config)
+		}
+		
+		exitConfig()
+	}
+	
+	func exitConfig() {
+		dismiss(animated: true, completion: nil)
 	}
 }
 
